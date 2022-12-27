081ac5dd8b9a514f1c2f73a15056b15280cae8bc
Iliandr123 <isapronov@tourmalinecore.com>
2022-12-27 09:16:00 +0500

    e2e: add new examples for other actions with metamask

--- tests/e2e/specs/loginToMetamask.js
+++ tests/e2e/specs/loginToMetamask.js
@@ -1,5 +1,5 @@
 describe('Metamask Extension tests', () => {
-  it('connect to DApp with Metamask extension', () => {
+  it('connect to DApp with Metamask extension example', () => {
     // посещаем сайт
     cy.visit('/');
     // кликаем на кнопку Connect
@@ -11,8 +11,13 @@ describe('Metamask Extension tests', () => {
     // убеждаемся, что UI изменился
   });
 
-  it('create transaction', () => {
+  it('create transaction example', () => {
     cy.get('#sendButton').click();
     cy.confirmMetamaskTransaction().should('be.true');
-  })
+  });
+
+  it('confirm signature example', () => {
+    cy.get('#ethSign').click();
+    cy.confirmMetamaskSignatureRequest().should('be.true');
+  });
 })
